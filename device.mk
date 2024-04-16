573c708abf2f2bda2950cc9aa519293c66703cba
SonicBSV <sonicbsv@gmail.com>
2024-04-16 07:15:35 +0300

    markw: switch to android.hardware.usb@1.3-service.basic

--- device.mk
+++ device.mk
@@ -451,7 +451,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service.basic
 
 # vndservicemanager
 PRODUCT_PACKAGES += \
