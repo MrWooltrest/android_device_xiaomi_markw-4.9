e41e9c2833738c596be7d66693d597b02c8bf7d2
Bruno Martins <bgcngm@gmail.com>
2024-06-19 12:41:18 +0200

    markw: Inherit from QTI and common Xiaomi FCMs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,8 @@ HWUI_COMPILE_FOR_PERF := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := hardware/qcom-caf/common/compatibility_matrix.xml
