b7fcb0fcac114b5714d7e146fa40afb0e05d726b
MrWooltrest <wooltrest@gmail.com>
2024-12-08 22:59:14 +0200

    markw: parts: Drop vibration intensity control

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -29,9 +29,6 @@ import androidx.preference.PreferenceManager;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.preferences.FileUtils;
 import org.lineageos.settings.soundcontrol.SoundControlSettings;
-import org.lineageos.settings.vibration.VibratorStrengthPreference;
-import org.lineageos.settings.vibration.VibratorSettings;
-import org.lineageos.settings.vibration.VibratorOverrideModeSwitch;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -52,15 +49,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         boolean enabled = false;
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-        enabled = sharedPrefs.getBoolean(VibratorSettings.PREF_VMAX_OVERRIDE_SWITCH, false);
-        restore(VibratorOverrideModeSwitch.getFile(), enabled);
 
         if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
             if (DEBUG) Log.d(TAG, "Starting Doze service");
             DozeUtils.startService(context);
         }
-
-        VibratorStrengthPreference.restore(context);
 
         int gain = Settings.Secure.getInt(context.getContentResolver(),
                 SoundControlSettings.PREF_HEADPHONE_GAIN, 4);
--- parts/src/org/lineageos/settings/PartsSearchIndexablesProvider.java
+++ parts/src/org/lineageos/settings/PartsSearchIndexablesProvider.java
@@ -34,7 +34,6 @@ import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUM
 import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
 
 import org.lineageos.settings.soundcontrol.SoundControlSettingsActivity;
-import org.lineageos.settings.vibration.VibratorActivity;
 
 import java.util.HashSet;
 
@@ -48,8 +47,6 @@ public class PartsSearchIndexablesProvider extends SearchIndexablesProvider {
         final int rank = 1, iconResId = 0;
         sResMap.add(new SearchIndexableResource(rank, R.xml.soundcontrol_settings,
                     SoundControlSettingsActivity.class.getName(), iconResId));
-        sResMap.add(new SearchIndexableResource(rank, R.xml.vibration_settings,
-                    VibratorActivity.class.getName(), iconResId));
     }
 
     @Override
