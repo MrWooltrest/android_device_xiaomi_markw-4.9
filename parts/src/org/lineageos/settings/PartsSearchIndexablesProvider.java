eec2e51f368f274e741fd397506e380f98e65ef0
MrWooltrest <wooltrest@gmail.com>
2024-12-08 21:47:51 +0200

    markw: parts: Drop torch settings

--- parts/src/org/lineageos/settings/PartsSearchIndexablesProvider.java
+++ parts/src/org/lineageos/settings/PartsSearchIndexablesProvider.java
@@ -34,7 +34,6 @@ import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUM
 import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
 
 import org.lineageos.settings.soundcontrol.SoundControlSettingsActivity;
-import org.lineageos.settings.torch.TorchSettingsActivity;
 import org.lineageos.settings.vibration.VibratorActivity;
 
 import java.util.HashSet;
@@ -49,8 +48,6 @@ public class PartsSearchIndexablesProvider extends SearchIndexablesProvider {
         final int rank = 1, iconResId = 0;
         sResMap.add(new SearchIndexableResource(rank, R.xml.soundcontrol_settings,
                     SoundControlSettingsActivity.class.getName(), iconResId));
-        sResMap.add(new SearchIndexableResource(rank, R.xml.torch_settings,
-                    TorchSettingsActivity.class.getName(), iconResId));
         sResMap.add(new SearchIndexableResource(rank, R.xml.vibration_settings,
                     VibratorActivity.class.getName(), iconResId));
     }
