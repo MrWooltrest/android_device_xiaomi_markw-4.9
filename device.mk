1efe94860a8129113391abbaae33b566c72242c0
MrWooltrest <wooltrest@gmail.com>
2024-12-25 13:36:29 +0200

    markw: Drop vendor.qti.hardware.display.mapper

--- device.mk
+++ device.mk
@@ -201,8 +201,6 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libvulkan \
     vendor.display.config@1.0.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.memtrack-service
 
 # Device-specific settings
